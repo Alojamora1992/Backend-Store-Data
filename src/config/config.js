25590283f4a0a5249c50bf2d6b27abd3a949260c
Carlos A. Posada <54514547+Carlos90012554607@users.noreply.github.com>
2023-07-20 17:44:34 -0500

    continue

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,8 @@ const config  = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
-  dbUri: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` //cambiar postgres por mysql cuando use mysql
+  dbUri: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, //cambiar postgres por mysql cuando use mysql
+  apiKey: process.env.API_KEY
 }
 
 module.exports = { config }
--- src/middlewares/error.handler.js
+++ src/middlewares/error.handler.js
@@ -5,13 +5,6 @@ function logErrors (err, req, res, next) {
   next(err);
 }
 
-function errorHandler(err, req, res, next) {
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack,
-  });
-}
-
 function boomErrorHandler(err, req, res, next) {
   if (err.isBoom) {
     const { output } = err;
@@ -19,6 +12,13 @@ function boomErrorHandler(err, req, res, next) {
   } else {
     next(err);
   }
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 }
 
 function ormErrorHandler(err, req, res, next) {
